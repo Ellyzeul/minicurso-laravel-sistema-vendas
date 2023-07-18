4e1d0cf0cda3ba5bdceac80438bbf7c3222caa83
Ellyzeul <gabrielaug.240@gmail.com>
2023-07-18 00:15:21 -0300

    chore: configuraçações de models e migrations

--- database/migrations/2023_07_18_030157_add_foreign_key_on_order_products_table.php
+++ database/migrations/2023_07_18_030157_add_foreign_key_on_order_products_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->foreign('id_order')->references('id')->on('orders');
+            $table->foreign('id_product')->references('id')->on('products');
         });
     }
 
@@ -23,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::table('order_products', function (Blueprint $table) {
+            //
+        });
     }
 };
